541a79187e9e85f7a1e8d2ea57f66d05ca37daff
Norbert305 <sealsnorbert@yahoo.com>
2023-07-30 15:52:14 -0400

    updates

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConverToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConverToSpacesPipe
-
+    ConverToSpacesPipe,
+    StarComponent
   ],
   imports: [
     BrowserModule, FormsModule
   ],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
