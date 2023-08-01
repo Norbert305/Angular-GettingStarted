3e1d8182b21794e16e783ebd09d71f525d01c0db
Norbert305 <sealsnorbert@yahoo.com>
2023-07-31 21:28:06 -0400

    changes

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -6,16 +6,29 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConverToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductionDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConverToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductionDetailComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
+      { path : 'products', component: ProductListComponent },
+      { path : "products/:id", canActivate: [ProductDetailGuard], component:ProductionDetailComponent},
+      { path : "products/:id", component: ProductionDetailComponent },
+      { path : "welcome", component: WelcomeComponent },
+      { path : "", redirectTo: "welcome", pathMatch: "full" },
+      { path : "**", redirectTo: "welcome", pathMatch: "full" }
+    ])
   ],
   bootstrap: [AppComponent]
 
